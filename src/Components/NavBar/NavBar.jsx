e6ab28c8b485bb037df23a3bb6708fc66cff4223
AndrewSRB <116013992+AndrewSRibeiro01@users.noreply.github.com>
2023-05-13 15:45:02 -0300

    deixando o codigo mais legivel, limpando codigo sujo e deixando compacto

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { patroa1 } from "../assets"
+import {  } from "../assets"
 import { Link } from "react-router-dom"
 import { H5, Button, Navbar, ImgPatroa } from "../../Estilizações/EHeader"
 import Map from "./Map"
